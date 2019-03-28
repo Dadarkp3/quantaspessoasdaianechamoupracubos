087ed3eb6f2d975fe267949b9fda1b28ebd45d8b
DaianeSilva <adm.daianesilva@gmail.com>
2019-03-28 19:58:03 -0300

    Projeto Cubos

---
+++ build/precache-manifest.ad31c3a33c55397ca1c29f368e74333f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "04641fecb1ab7bc8e917",
+    "url": "/quantaspessoasdaianechamoupracubos/static/css/main.3e438468.chunk.css"
+  },
+  {
+    "revision": "04641fecb1ab7bc8e917",
+    "url": "/quantaspessoasdaianechamoupracubos/static/js/main.5914b846.chunk.js"
+  },
+  {
+    "revision": "6ec5d7521f188423a9f8",
+    "url": "/quantaspessoasdaianechamoupracubos/static/js/runtime~main.d842bb7e.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/quantaspessoasdaianechamoupracubos/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quantaspessoasdaianechamoupracubos/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "02f333486fa5859420f790c786348840",
+    "url": "/quantaspessoasdaianechamoupracubos/index.html"
+  }
+];
